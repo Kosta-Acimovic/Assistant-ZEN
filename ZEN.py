bcca52b30940990fec0a3c53f3ebffa737f4cbf8
Kosta <kosta.acimovic@outlook.com>
2023-10-30 23:05:12 +0100

    Basic functions and voice control

---
+++ ZEN.py
@@ -0,0 +1,92 @@
+import pyttsx3 #pip install pyttsx3
+import datetime
+import speech_recognition as sr #pip install SpeechRecognition
+
+engine = pyttsx3.init()
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak(Time)
+
+
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    if month == 1:
+        month = "January"
+    elif month == 2:
+        month = "February"
+    elif month == 3:
+        month = "March"
+    elif month == 4:
+        month = "April"
+    elif month == 5:
+        month = "May"
+    elif month == 6:
+        month = "June"
+    elif month == 7:
+        month = "July"
+    elif month == 8:
+        month = "August"
+    elif month == 9:
+        month = "September"
+    elif month == 10:
+        month = "October"
+    elif month == 11:
+        month = "November"
+    else:
+        month = "December"
+
+    day = int(datetime.datetime.now().day)
+    speak(day)
+    speak(month)
+    speak(year)
+
+
+def wishme():
+    speak("Welcome back!")
+    speak("the current time is")
+    time()
+    speak("and current date is ")
+    date()
+
+
+    hour = datetime.datetime.now().hour
+    if hour>=6 and hour<12 :
+        speak("Good morning!")
+    elif hour>=12 and hour<18:
+        speak("Good afternoon!")
+    elif hour>=18 and hour<24:
+        speak("Good evening!")
+    else:
+        speak("Good night!")
+
+
+    speak("ZEN at your service. Please tell me how can i help you")
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening.....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(query)
+
+    except Exception as e:
+        print(e)
+        speak("Say that again please...")
+
+        return "None"
+    return query
+
+takeCommand()
---
+++ main.py
@@ -0,0 +1,3 @@
+import ZEN
+
+ZEN.takeCommand()
